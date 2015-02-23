rooney/prezto

commit 933c61b8f1af64e745260f3c92d32fa08399688d

[Fix #776] Support GNU top

modules/utility/init.zsh
@@ -146,8 +146,13 @@ alias du='du -kh'
 if (( $+commands[htop] )); then
   alias top=htop
 else
-  alias topc='top -o cpu'
-  alias topm='top -o vsize'
+  if [[ "$OSTYPE" == darwin*|*bsd* ]]; then
+    alias topc='top -o cpu'
+    alias topm='top -o vsize'
+  else
+    alias topc='top -o %CPU'
+    alias topm='top -o %MEM'
+  fi
 fi
 
 # Miscellaneous

commit 163c378ecf1b4bed789be11d93e9eeb5bf2fd667

Add missing parenthesis
The bug was introduced in 933c61b.

modules/utility/init.zsh
@@ -146,7 +146,7 @@ alias du='du -kh'
 if (( $+commands[htop] )); then
   alias top=htop
 else
-  if [[ "$OSTYPE" == darwin*|*bsd* ]]; then
+  if [[ "$OSTYPE" == (darwin*|*bsd*) ]]; then
     alias topc='top -o cpu'
     alias topm='top -o vsize'
   else